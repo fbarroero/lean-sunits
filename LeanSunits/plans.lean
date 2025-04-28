a186b14724d5e578a976a232c97f8bcce4a2dd09
Lars Kuehne <lars.kuehne@ucd.ie>
2025-04-27 22:10:56 -0400

    progress

--- LeanSunits/plans.lean
+++ LeanSunits/plans.lean
@@ -17,21 +17,29 @@ variable
 -- M = (union of primes in S)^c = (intersection of complements)
 
 def M : Submonoid R := {
-  carrier := (⋂ (v : HeightOneSpectrum R) (_ : v ∈ S), (v.asIdeal.carrier)ᶜ)
+  carrier := (⋂ (v : HeightOneSpectrum R) (_ : v ∉ S), (v.asIdeal.carrier)ᶜ)
   mul_mem' := by
     simp only [Submodule.carrier_eq_coe, mem_iInter, mem_compl_iff, SetLike.mem_coe]
     intro a b ha hb v hv
     specialize ha v hv
     specialize hb v hv
-
-    sorry
-  one_mem' := sorry
+    refine Ideal.IsPrime.mul_not_mem ?_ ha hb
+    exact v.isPrime
+  one_mem' := by
+    simp only [Submodule.carrier_eq_coe, mem_iInter, mem_compl_iff, SetLike.mem_coe]
+    intro v hv
+    refine (Ideal.ne_top_iff_one v.asIdeal).mp ?_
+    exact Ideal.IsPrime.ne_top'
 }
 
+instance : IsLocalization (M S) <| S.integer K where
+  map_units' := by
+    simp only [M, Submodule.carrier_eq_coe, Subtype.forall, Submonoid.mem_mk, Subsemigroup.mem_mk,
+      mem_iInter, mem_compl_iff, SetLike.mem_coe]
+    intro r hr
 
-
-instance : IsLocalization (R := R) (M S) <| S.integer K where
-  map_units' := sorry
+    --have (x :   (S.integer K)ˣ) :
+    sorry
   surj' := sorry
   exists_of_eq := sorry
 
